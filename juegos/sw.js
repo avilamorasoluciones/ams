582cc9da8318e1df464ed8eb01c5b42958075add
Diego Avila <avilardiegoa@gmail.com>
2026-09-26 02:38:03 -0500

    Actualizar caché para versión visual final de los juegos

--- juegos/sw.js
+++ juegos/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "juegos-avila-mora-v12";
+const CACHE_NAME = "juegos-avila-mora-v13";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -9,7 +9,7 @@ const APP_SHELL = [
   "./tabu.html",
   "./verdadreto.html",
   "./yonunca.html",
-  "./styles.css?v=20260926-4",
+  "./styles.css?v=20260926-5",
   "./scripts.js",
   "./players.js",
   "./game-bridge.js",
