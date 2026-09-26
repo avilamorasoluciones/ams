eec8d74300724de60d0a34fbf395b7916485926d
Diego Avila <avilardiegoa@gmail.com>
2026-09-26 02:19:04 -0500

    Actualizar caché PWA tras ajustes móviles

--- juegos/sw.js
+++ juegos/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "juegos-avila-mora-v8";
+const CACHE_NAME = "juegos-avila-mora-v9";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -9,7 +9,7 @@ const APP_SHELL = [
   "./tabu.html",
   "./verdadreto.html",
   "./yonunca.html",
-  "./styles.css?v=20260926",
+  "./styles.css?v=20260926-2",
   "./scripts.js",
   "./players.js",
   "./game-bridge.js",
